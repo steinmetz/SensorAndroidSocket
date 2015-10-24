1dbc36944edf21367e5688b6285527ccef256322
charles <charlessteinmetzk@gmail.com>
2015-10-24 13:16:03 -0200

    Sensor com start/stop, udp e tcp. Primeira versao para testes

--- app/src/main/java/com/trabalho/protocolos/sensor/MainActivity.java
+++ app/src/main/java/com/trabalho/protocolos/sensor/MainActivity.java
@@ -1,18 +1,18 @@
 package com.trabalho.protocolos.sensor;
 
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.Formatter;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.trabalho.protocolos.sensor.socket.TCPServerSocket;
-
 public class MainActivity extends AppCompatActivity  {
 
-    private TextView tv;
+    private TextView txtValue;
     private TextView txtStatus;
 
     Button btnStart;
@@ -27,32 +27,70 @@ public class MainActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv = (TextView) findViewById(R.id.tv);
+        txtValue = (TextView) findViewById(R.id.txtValue);
         txtStatus = (TextView) findViewById(R.id.txtStatus);
         btnStart = (Button) findViewById(R.id.btnStart);
         spnTCPUPD = (Spinner) findViewById(R.id.spnTCPUPD);
         edtPorta = (EditText) findViewById(R.id.edtPorta);
 
 
-        sensor = new Sensor(this);
+        sensor = new Sensor(this,txtStatus, txtValue);
+        sensor.startSensor();
 
+        WifiManager wm = (WifiManager) getSystemService(WIFI_SERVICE);
+        String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+
+        TextView txtIP = (TextView) findViewById(R.id.txtIP);
+        txtIP.setText(ip);
 
     }
 
     public void start(View v){
-        int porta = 9000;
-        try {
-            porta = Integer.parseInt(edtPorta.getText().toString());
-        }catch (Exception e){
-            txtStatus.setText("Porta inválida");
-            return;
+        if (v.getTag().equals("start")){
+            try {
+                final int porta = Integer.parseInt(edtPorta.getText().toString());
+                final String type = spnTCPUPD.getSelectedItem().toString();
+                Runnable run = new Runnable() {
+                    @Override
+                    public void run() {
+                        sensor.startServer(porta, type);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                txtStatus.setText("Servidor fechou.");
+                                setEnabledViews(true);
+                                btnStart.setEnabled(true);
+                            }
+                        });
+                    }
+                };
+                Thread t = new Thread(run);
+                txtStatus.setText("Servidor rodando...");
+                t.start();
+                setEnabledViews(false);
+            }catch (Exception e){
+                txtStatus.setText("Porta inválida");
+                return;
+            }
+        }else{
+            sensor.stopServer();
+            txtStatus.setText("Servidor ainda está rodando até a próxima conexão.");
+            btnStart.setEnabled(false);
         }
-        if(spnTCPUPD.getSelectedItem().toString().equals("TCP")){
-            TCPServerSocket socket = new TCPServerSocket(porta);
-            //socket.start(sensor);
-        }else {
 
+    }
+
+    public void setEnabledViews(boolean isEnabled){
+        spnTCPUPD.setEnabled(isEnabled);
+        edtPorta.setEnabled(isEnabled);
+        if (isEnabled){
+            btnStart.setText("Start");
+            btnStart.setTag("start");
+        }else{
+            btnStart.setText("Stop");
+            btnStart.setTag("stop");
         }
     }
+
 
 }
--- app/src/main/java/com/trabalho/protocolos/sensor/Sensor.java
+++ app/src/main/java/com/trabalho/protocolos/sensor/Sensor.java
@@ -4,6 +4,18 @@ import android.content.Context;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
 
 /**
  * Created by charles on 23/10/15.
@@ -12,15 +24,31 @@ public class Sensor implements SensorEventListener {
 
     private float value = 0;
     private SensorManager sManager;
+    private TextView txtLog;
+    private TextView txtValue;
+    private boolean runningServer = true;
 
-    public Sensor(Context ctx){
+
+    public Sensor(Context ctx) {
         sManager = (SensorManager) ctx.getSystemService(ctx.SENSOR_SERVICE);
+    }
+
+    public Sensor(Context ctx, TextView txtLog, TextView txtValue) {
+        sManager = (SensorManager) ctx.getSystemService(ctx.SENSOR_SERVICE);
+        this.txtLog = txtLog;
+        this.txtValue = txtValue;
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
         //Orientation X (Roll)
-        value = event.values[2];
+        if (Math.abs(value - event.values[2]) > 2) {
+            value = event.values[2];
+            if (txtValue != null) {
+                txtValue.setText("X: " + getValue());
+            }
+        }
+
     }
 
     @Override
@@ -28,17 +56,77 @@ public class Sensor implements SensorEventListener {
 
     }
 
-    protected void start() {
+    protected void startSensor() {
         sManager.registerListener(this, sManager.getDefaultSensor(android.hardware.Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_FASTEST);
     }
 
-    protected void stop() {
+    protected void stopSensor() {
         sManager.unregisterListener(this);
     }
 
-    public float getValue(){
+    public float getValue() {
         return value;
     }
 
+    public void startServer(int port, String type) {
+        runningServer = true;
+        if (type.equals("TCP")) {
+            ServerSocket serverSocket = null;
+
+            try {
+                serverSocket = new ServerSocket(port);
+            } catch (IOException e) {
+                txtLog.setText("Could not listen on port:" + port);
+                System.exit(1);
+            }
+
+            Socket socket = null;
+
+            try {
+                while (runningServer) {
+                    socket = serverSocket.accept();
+
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(),
+                            true);
+                    out.println(getValue());
+                    out.close();
+                }
+            } catch (IOException e) {
+                System.exit(1);
+            }
+            try {
+                socket.close();
+                serverSocket.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            // UDP
+        } else {
+            try {
+                DatagramSocket serverSocket = new DatagramSocket(port);
+                byte[] receiveData = new byte[10];
+                byte[] sendData = new byte[10];
+                while(runningServer)
+                {
+                    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                    serverSocket.receive(receivePacket);
+                    String sentence = new String( receivePacket.getData());
+                    InetAddress IPAddress = receivePacket.getAddress();
+                    int portCli = receivePacket.getPort();
+                    sendData = String.valueOf(getValue()).getBytes();
+                    DatagramPacket sendPacket =
+                            new DatagramPacket(sendData, sendData.length, IPAddress, portCli);
+                    serverSocket.send(sendPacket);
+                }
+                serverSocket.close();
+            } catch (Exception e) {
+                txtLog.setText(e.getMessage());
+            }
+        }
+    }
+
+    public void stopServer() {
+        runningServer = false;
+    }
 
 }
